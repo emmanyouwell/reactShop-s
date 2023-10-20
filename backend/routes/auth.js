b7529fed87745b060374e559ebc5ae1285710335
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-20 08:30:21 +0800

    multer

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const upload = require("../utils/multer");
 
 const {registerUser, loginUser, logout, forgotPassword, resetPassword, updatePassword, getUserProfile, updateProfile, allUsers, getUserDetails} = require('../controllers/authController');
 
 const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth')
 
-router.post('/register', registerUser);
+router.post('/register', upload.single("avatar"), registerUser);
 router.post('/login', loginUser);
 router.get('/logout', logout);
 router.post('/password/forgot', forgotPassword);
