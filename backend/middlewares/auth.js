857ed6ddbf3986a4f454ed44cd6726a39bae0722
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-20 11:32:36 +0800

    profile update

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -10,7 +10,6 @@ exports.isAuthenticatedUser = async (req, res, next) => {
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
     req.user = await User.findById(decoded.id);
-    
     next()
 };
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import ProductDetails from "./Components/Product/ProductDetails";
 import Login from "./Components/User/Login";
 import Register from './Components/User/Register';
 import Profile from "./Components/User/Profile";
+import UpdateProfile from "./Components/User/UpdateProfile";
 function App() {
 
   return (
@@ -21,6 +22,8 @@ function App() {
           <Route path="/login" element={<Login />} exact="true" />
           <Route path="/register" element={<Register />} exact="true" />
           <Route path="/me" element={<Profile />} exact="true" />
+          <Route path="/me/update"  element={<UpdateProfile />} exact="true" />
+       
         </Routes>
       </Router>
       <Footer />
