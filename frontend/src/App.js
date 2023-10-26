1f36790100e7f9800fa2a25690cbc41c5fea9f4d
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-26 12:02:57 +0800

    forgot password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Register from './Components/User/Register';
 import Profile from "./Components/User/Profile";
 import UpdateProfile from "./Components/User/UpdateProfile";
 import UpdatePassword from "./Components/User/UpdatePassword";
+import ForgotPassword from "./Components/User/ForgotPassword";
 function App() {
 
   return (
@@ -23,8 +24,9 @@ function App() {
           <Route path="/login" element={<Login />} exact="true" />
           <Route path="/register" element={<Register />} exact="true" />
           <Route path="/me" element={<Profile />} exact="true" />
-          <Route path="/me/update"  element={<UpdateProfile />} exact="true" />
+          <Route path="/me/update" element={<UpdateProfile />} exact="true" />
           <Route path="/password/update" element={<UpdatePassword />} />
+          <Route path="/password/forgot" element={<ForgotPassword />} exact="true" />
         </Routes>
       </Router>
       <Footer />
