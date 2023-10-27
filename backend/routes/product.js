951df8a5593614c0f3af515baf7061d6055424cb
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-27 11:38:21 +0800

    update product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -9,6 +9,6 @@ const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth')
 router.post('/admin/product/new',  isAuthenticatedUser, upload.array('images', 10), newProduct)
 router.get('/products',  getProducts)
 router.get('/product/:id', getSingleProduct);
-router.route('/admin/product/:id',isAuthenticatedUser, ).put(updateProduct).delete(deleteProduct);
+router.route('/admin/product/:id',isAuthenticatedUser, ).put(upload.array('images', 10), updateProduct).delete(deleteProduct);
 router.get('/admin/products', isAuthenticatedUser, getAdminProducts);
 module.exports = router;
