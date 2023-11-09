4ef19a604b3b2210fd708946c490019cf0cef383
rommel dalisay <rommelmdalisay@gmail.com>
2023-11-09 14:43:57 +0800

    protected route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -125,21 +125,21 @@ function App() {
           <Route path="/orders/me" element={<ListOrders />} />
           <Route path="/order/:id" element={<OrderDetails />} />
           {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-          <Route path="/admin/products" element={<ProductsList />} />
+          {/* <Route path="/admin/products" element={<ProductsList />} /> */}
           <Route path="/admin/product" element={<NewProduct />} />
           <Route
             path="/admin/product/:id"
             element={<UpdateProduct />} />
-          <Route
+          {/* <Route
             path="/admin/orders"
             element={<OrdersList />}
-          />
+          /> */}
           <Route
             path="/admin/order/:id"
             element={<ProcessOrder />} />
-          <Route
+          {/* <Route
             path="/admin/users"
-            element={<UsersList />} />
+            element={<UsersList />} /> */}
           <Route path="/admin/user/:id" element={<UpdateUser />} />
           <Route
             path="/dashboard"
@@ -149,6 +149,31 @@ function App() {
               </ProtectedRoute>
             }
           />
+          <Route
+            path="/admin/products"
+            element={
+              <ProtectedRoute isAdmin={true}>
+                <ProductsList />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/users"
+            element={
+              <ProtectedRoute isAdmin={true}>
+                <UsersList />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/orders"
+            element={
+              <ProtectedRoute isAdmin={true}>
+                <OrdersList />
+              </ProtectedRoute>
+            }
+          />
+          
         </Routes>
       </Router>
       <Footer />
