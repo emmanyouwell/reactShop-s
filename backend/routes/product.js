3fbeee310d594606a2e2071557f7e00ae3073e6c
rommel dalisay <rommelmdalisay@gmail.com>
2023-11-09 14:49:32 +0800

    review update

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../utils/multer')
 
-const {newProduct, getProducts, getSingleProduct, updateProduct, deleteProduct, getAdminProducts} = require('../controllers/productController');
+const { newProduct, getProducts, getSingleProduct, updateProduct, deleteProduct, getAdminProducts, createProductReview, getProductReviews, deleteReview } = require('../controllers/productController');
 
-const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth')
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
 
-router.post('/admin/product/new',  isAuthenticatedUser, upload.array('images', 10), newProduct)
-router.get('/products',  getProducts)
+router.post('/admin/product/new', isAuthenticatedUser, upload.array('images', 10), newProduct)
+router.get('/products', getProducts)
 router.get('/product/:id', getSingleProduct);
-router.route('/admin/product/:id',isAuthenticatedUser, ).put(upload.array('images', 10), updateProduct).delete(deleteProduct);
+router.route('/admin/product/:id', isAuthenticatedUser,).put(upload.array('images', 10), updateProduct).delete(deleteProduct);
 router.get('/admin/products', isAuthenticatedUser, authorizeRoles('admin'), getAdminProducts);
+router.put('/review', isAuthenticatedUser, createProductReview);
+router.get('/reviews', isAuthenticatedUser, getProductReviews)
+router.delete('/reviews', isAuthenticatedUser, authorizeRoles('admin'), deleteReview)
 module.exports = router;
