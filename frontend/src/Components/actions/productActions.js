83cd87a1da80adcfda3fcade1ab2e8b227e5fdc0
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-03 15:14:26 +0800

    product actions

--- frontend/src/Components/actions/productActions.js
+++ frontend/src/Components/actions/productActions.js
@@ -1,34 +1,42 @@
 import axios from 'axios';
 
-import { ALL_PRODUCTS_REQUEST,
-		 ALL_PRODUCTS_SUCCESS, 
-		 ALL_PRODUCTS_FAIL,
-		 CLEAR_ERRORS 
-		} from '../constants/productConstants';
+import {
+    ALL_PRODUCTS_REQUEST,
+    ALL_PRODUCTS_SUCCESS,
+    ALL_PRODUCTS_FAIL,
+    CLEAR_ERRORS
+} from '../constants/productConstants';
 
-export const getProducts = () => async (dispatch) => {
-	try {
-		dispatch({
-			type: ALL_PRODUCTS_REQUEST
-		})
-			const {data} = await axios.get('/api/v1/products')
-		
-		dispatch({
-			type: ALL_PRODUCTS_SUCCESS,
-			payload: data
-		})
+export const getProducts = (page = 1, keyword = '', price, category = '') => async (dispatch) => {
+    try {
+        dispatch({
+            type: ALL_PRODUCTS_REQUEST
+        })
+        let link = ''
 
-	} catch(error) {
-		dispatch({
-			type: ALL_PRODUCTS_FAIL,
-			payload: error.response.data.message
-		})
-	}
+        link = `${process.env.REACT_APP_API}/api/v1/products/?page=${page}&keyword=${keyword}&price[lte]=${price[1]}&price[gte]=${price[0]}`
+
+        if (category) {
+            link = `${process.env.REACT_APP_API}/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}&category=${category}`
+        }
+        const { data } = await axios.get(link)
+
+        dispatch({
+            type: ALL_PRODUCTS_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type: ALL_PRODUCTS_FAIL,
+            payload: error.response.data.message
+        })
+    }
 }
 
-export const clearErrors = () => async (dispatch) =>{
-	dispatch({
-		type: CLEAR_ERRORS
+export const clearErrors = () => async (dispatch) => {
+    dispatch({
+        type: CLEAR_ERRORS
 
-	})
+    })
 }
