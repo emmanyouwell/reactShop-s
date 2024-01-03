1e9f91523749e46c6041c8a8b4c67e8b84673201
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-03 14:51:49 +0800

    provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'
+import store from './store'
 
 import App from './App';
 import { ToastContainer } from 'react-toastify';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <ToastContainer />
+    <Provider store={store} >
+      <App />
+      <ToastContainer />
+    </Provider>
   </React.StrictMode>
 );
 
