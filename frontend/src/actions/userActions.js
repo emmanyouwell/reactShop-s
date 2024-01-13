b931c884eea23daa7a0349398e012d7eacea9774
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-13 08:59:54 +0800

    user load

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -104,6 +104,28 @@ export const logout = () => async (dispatch) => {
         })
     }
 }
+
+export const loadUser = () => async (dispatch) => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            withCredentials: true,
+        }
+        dispatch({ type: LOAD_USER_REQUEST })
+        const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/me`,config)
+        dispatch({
+            type: LOAD_USER_SUCCESS,
+            payload: data.user
+        })
+    } catch (error) {
+        dispatch({
+            type: LOAD_USER_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
 export const clearErrors = () => async (dispatch) => {
 	dispatch({
 		type: CLEAR_ERRORS
