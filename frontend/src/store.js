e890b16ccb39218ef4661f05f617c93cb9e4fe50
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-17 16:51:31 +0800

    cart initial state

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -16,7 +16,21 @@ const reducer = combineReducers({
 
 })
 
+// let initialState = {
+
+// }
+
 let initialState = {
+    cart: {
+        cartItems: localStorage.getItem('cartItems')
+            ? JSON.parse(localStorage.getItem('cartItems'))
+            : [],
+        shippingInfo: localStorage.getItem('shippingInfo')
+             ? JSON.parse(localStorage.getItem('shippingInfo'))
+            : {}
+    },
+    user: {},
+    auth: {},
 
 }
 
