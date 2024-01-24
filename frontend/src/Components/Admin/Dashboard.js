49d8fe72464cdfe2e2078b93fbde04a92d66bdae
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-24 15:50:18 +0800

    user dashboard

--- frontend/src/Components/Admin/Dashboard.js
+++ frontend/src/Components/Admin/Dashboard.js
@@ -14,10 +14,12 @@ import MonthlySalesChart from './MonthlySalesChart'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAdminProducts } from '../../actions/productActions'
 import { allOrders } from '../../actions/orderActions'
+import { allUsers } from '../../actions/userActions';
 const Dashboard = () => {
     const dispatch = useDispatch();
     const { products, loading } = useSelector(state => state.products)
     const { orders, totalAmount, } = useSelector(state => state.allOrders)
+    const { error, users } = useSelector(state => state.allUsers)
     // const [products, setProducts] = useState([])
     // const [error, setError] = useState('')
     // const [users, setUsers] = useState([])
@@ -52,7 +54,7 @@ const Dashboard = () => {
     useEffect(() => {
         dispatch(getAdminProducts())
         dispatch(allOrders())
-        // allUsers()
+       dispatch(allUsers())
     }, [dispatch])
 
     return (
@@ -115,9 +117,9 @@ const Dashboard = () => {
                                 <div className="col-xl-3 col-sm-6 mb-3">
                                     <div className="card text-white bg-info o-hidden h-100">
 
-                                        {/* <div className="card-body">
+                                        <div className="card-body">
                                             <div className="text-center card-font-size">Users<br /> <b>{users && users.length}</b></div>
-                                        </div> */}
+                                        </div>
 
                                         <Link className="card-footer text-white clearfix small z-1" to="/admin/users">
                                             <span className="float-left">View Details</span>
